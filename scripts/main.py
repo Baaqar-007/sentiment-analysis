10b2b898af9c1c748f1c10986869fea1c8f1518e
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2024-04-30 17:57:38 +0530

    Added basic NLTK setup

---
+++ scripts/main.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as ssn 
+
+
+plt.style.use('ggplot')
+
+import nltk
+
+#Reading data 
+df = pd.read_csv('../data/Reviews.csv') # ".." to move up a directory then search
+
+#print(df.head())
+
+df = df.head(500) # getting the first 500 test 
+
+
+"""Checking the data distribution:
+
+ax = df['Score'].value_counts().sort_index().plot(kind='bar', 
+	title ='Count of Reviews by Stars',
+	figsize = (10,5))
+
+ax.set_xlabel('Review Stars')
+plt.show()
+
+Mostly positive reviews""" 
+
+example = df['Text'][50]
+print(example)
+
+#Tokenising 
+tokens = nltk.word_tokenize(example) # splits it into tokens ("words")
+
+tags = nltk.pos_tag(tokens[:10]) # part of speech tags
+print(tags)
+
+entity = nltk.chunk.ne_chunk(tags)
+print(entity.pprint())
+
