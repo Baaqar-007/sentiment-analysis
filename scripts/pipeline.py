7382efd8e53e2670a2369e2d838e63059b0dfac8
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2024-05-08 23:48:09 +0530

    Currently not functioning as expected

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -3,4 +3,4 @@ import pyperclip as ppc
 
 sent_pipeline = pipeline("sentiment-analysis")
 
-print("Analysid of the copied data:", sent_pipeline(ppc.paste()))
+print("Analysis of the copied data:", sent_pipeline(ppc.paste()))
